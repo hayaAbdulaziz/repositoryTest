634adaf59afe39a1ea8b53b1fb46dd628cdcaffb
Haya Binkhathran <hayal5066@gmail.com>
2021-07-01 10:49:44 +0300

    first commit

---
+++ second.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View , Image , TextInput} from 'react-native';
+import React from 'react';
+export default function Second() {
+    return (
+      <View style={styles.container}>
+
+       <Text>
+           -
+       </Text>
+  
+  
+      </View>
+      
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      // flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      top: 10 ,
+      left:50 ,
+      right: 50 ,
+      
+    },
+  
+  });
